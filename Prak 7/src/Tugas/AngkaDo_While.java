36a72f32745fe51bd942710da18769ee3c39ca41
21343055 <112170659+21343055@users.noreply.github.com>
2022-10-04 18:10:15 +0700

    Add files via upload

---
+++ Prak 7/src/Tugas/AngkaDo_While.java
@@ -0,0 +1,19 @@
+/*
+ * Nama : Muhamad Fathur Rahman
+ * NIM  : 21343055
+ * Prodi: Teknik Informatika
+ */
+package Tugas;
+
+public class AngkaDo_While {
+    public static void main(String[] args) {
+        int i = 100;
+
+        do
+        {
+            System.out.println(i);
+            i--;
+        }
+        while (i >= 1);
+    }
+}
---
+++ Prak 7/src/Tugas/AngkaWhile.java
@@ -0,0 +1,18 @@
+/*
+ * Nama : Muhamad Fathur Rahman
+ * NIM  : 21343055
+ * Prodi: Teknik Informatika
+ */
+package Tugas;
+
+public class AngkaWhile {
+    public static void main(String[] args) {
+        int i = 100;
+
+        while (i >= 1)
+        {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
